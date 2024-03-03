3e3a2fcb1c19cb63d54abc3b365d115613f1dcd0
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-03-02 22:43:02 -0500

    added a stateless widget so flutter does a hot reload after saving

--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,44 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: const Text('hot reload please'),
-      centerTitle: true,
-      backgroundColor: Colors.green[700],
-    ),
-    body:  Center(
-      child:  Text(
-          'Hello Monkey',
-          style: TextStyle(
-          fontSize: 30.0,
-          fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            backgroundColor: Colors.green[200],
-            height: 5.0,
-            fontFamily: 'IndieFlower'
-          ),
-      ),
-
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        // Add your onPressed logic here
-        print('FloatingActionButton pressed!');
-      },
-      tooltip: 'Increment Counter',
-      child: const Text('Click'),
-      backgroundColor: Colors.green[300],
-    ),
-  ),
+  home: const Home(),
 ));
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('hot reload please'),
+        centerTitle: true,
+        backgroundColor: Colors.green[700],
+      ),
+      body:  Center(
+        child:  Text(
+          'Hot restart is working',
+          style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              backgroundColor: Colors.green[200],
+              height: 5.0,
+              fontFamily: 'IndieFlower'
+          ),
+        ),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed logic here
+          print('FloatingActionButton pressed!');
+        },
+        tooltip: 'Increment Counter',
+        child:  Text('Click'),
+        backgroundColor: Colors.green[300],
+      ),
+    );
+  }
+}
